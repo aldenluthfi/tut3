e1086de7ae083e4df897a051abe87d09d2926872
aldenluthfi <aldenluthfi@gmail.com>
2025-03-14 19:42:33 +0700

    tut5

--- scenes/Tower.gd
+++ scenes/Tower.gd
@@ -9,3 +9,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func _on_body_entered(body):
+	if body.get_name() == "Player":
+		get_tree().change_scene_to_file(str("res://scenes/WinScreen.tscn"))
